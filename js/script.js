c4b8a37b4392f53b6f7a3e934643dd48976833f0
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-02 15:38:11 +0200

    [UPD] define an index variable for the while cycle

--- js/script.js
+++ js/script.js
@@ -1,2 +1,4 @@
 // DEFINISCO UN'ARRAY CON LA LISTA DELLA SPESA
 let shoppingList = ['mele', 'anguria', 'banane', 'broccoli', 'pasta', 'pomodori', 'cipolle', 'patate'];
+// DEFINISCO LA VARIABILE DELL'INDICE PER IL CICLO WHILE
+let i = 0;
