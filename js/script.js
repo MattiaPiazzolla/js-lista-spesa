46848d40aca9531e51a90cb73008a39c03f72bc6
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-02 15:39:09 +0200

    [UPD] defined the while cycle

--- js/script.js
+++ js/script.js
@@ -2,3 +2,7 @@
 let shoppingList = ['mele', 'anguria', 'banane', 'broccoli', 'pasta', 'pomodori', 'cipolle', 'patate'];
 // DEFINISCO LA VARIABILE DELL'INDICE PER IL CICLO WHILE
 let i = 0;
+// DEFINISCO IL CICLO WHILE
+while (i < shoppingList.length){
+    
+}
