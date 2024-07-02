6cf8ccaaf5151663fe082ab166274ce0dac2c23c
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-02 16:30:47 +0200

    [UPD] add a text content for the li element created

--- js/script.js
+++ js/script.js
@@ -8,6 +8,9 @@ const listDestination = document.getElementById('list');
 while (i < shoppingList.length){
     // DEFINISCO UN NUOVO ELEMENTO PER OGNI ELEMENTO DELL'ARRAY CICLATO
     const liElement = document.createElement('li');
+    // AGGIUNGO IL CONTENUTO TESTUALE AGLI ELEMENTI LI CREATI 
+    liElement.innerText = shoppingList[i];
     
+    // INTERROMPO IL CICLO //chiedere la definizione giusta per questo passaggio 
     i ++
 }
